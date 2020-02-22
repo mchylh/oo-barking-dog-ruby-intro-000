a71b1924e100e1542cc50d93ef78fed9d605782f
Michaylah Mam <mchylh@gmail.com>
2020-02-22 23:03:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Dog
+  def bark
+    puts "Woof!"
+  end
+ end
+
+ fido = Dog.new      #initializing a new object/instance
+ fido.bark
+   # => "Woof!"    #call instance method bark on object fido
