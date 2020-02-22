dbd98c94eed2c27d6f02ba01b7119a5d5e12c908
Michaylah Mam <mchylh@gmail.com>
2020-02-22 23:21:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,6 +8,6 @@ class Dog
   end
 end
 
- fido = Dog.new("Fido")      #initializing a new object/instance
+ fido.name     #initializing a new object/instance
  fido.bark
    # => "Woof!"    #call instance method bark on object fido
